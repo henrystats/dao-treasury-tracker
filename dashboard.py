7341c53b232093c72457fc9ae5e6e6d97ef1a1db
henrystats <65398020+henrystats@users.noreply.github.com>
2025-06-13 23:46:12 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,7 @@ SA_INFO    = json.loads(st.secrets["gcp_service_account"])
 CHAIN_IDS   = ["eth", "arb", "base", "scrl", "avax", "era", "bsc", "op", "linea", "corn", "zircuit", "bera", "blast", "swell", "uni", "sonic", "hyper"]
 CHAIN_NAMES = {"eth":"Ethereum","arb":"Arbitrum","base":"Base","scrl":"Scroll","avax":"Avalanche","era":"zkSync Era","bsc":"BNB Chain","op":"Optimism",
                 "linea":"Linea","corn":"Corn","zircuit":"Zircuit","bera":"Berachain","blast":"Blast","swell":"SwellChain","uni":"Unichain",
-               "sonic":"Sonic","hyper":"Hyperliquid
+               "sonic":"Sonic","hyper":"Hyperliquid"
             }
 headers     = {"AccessKey": ACCESS_KEY}
 
